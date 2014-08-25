317a83ab9c9dfd9951f498a892b40385d326cdf5
Mauricio Gamboa <mgamboa@mobiquityinc.com>
2014-08-24 23:41:23 -0600

    Contact Errors

--- public/js/app.js
+++ public/js/app.js
@@ -48,10 +48,46 @@ $(document).ready(function() {
       }
     });
   });
+  
+
   $('#myModal').on('hide.bs.modal', function(e) { 
     var player=$(e.target).find('iframe'),
     vidSrc=player.prop('src');
     player.prop('src','');
     player.prop('src',vidSrc);
   });
+
+  $('#contact-form form').validate({ errorElement: 'span'});
+  $('#subscribe-form').validate({ errorElement: 'span'});
+
+  
+  // Function to create the google map in the contact page
+  function initialize() {
+    var map_canvas = document.getElementById('map');
+
+    var latlng = new google.maps.LatLng(51.2374507,-0.5300000);
+    
+    var map_options = {
+      zoom: 15,
+      center: latlng,
+      scrollwheel: false,
+      scaleControl: false,
+      streetViewControl: false,
+      draggable: true, 
+      mapTypeControl: false,
+      zoomControl: false,
+      panControl: false
+    }
+    
+    var map = new google.maps.Map(map_canvas, map_options)
+
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      icon: 'public/images/marker.png'
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 });
